35ed1f0ce104d03b7d5119f24a4492589ac4e021
Peter Borza <borzapeter@yahoo.com>
2024-09-21 09:34:49 +0300

    CC-5: Navbar title, styling fixes, new game button

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -13,23 +13,21 @@ const Palette: FC<Props> = ({ colors, emptySlotsCount = 4 }) => {
   const fillSlots = () => colors ?? new Array(emptySlotsCount).fill(null);
 
   return (
-    <Stack>
-      <Paper elevation={5} sx={{ alignSelf: "start" }}>
-        <Stack
-          direction="row"
-          spacing={2}
-          p={1}
-          sx={{
-            bgcolor: "primary.main",
-            borderRadius: 1,
-          }}
-        >
-          {fillSlots().map(color => (
-            <ColorBall key={uuid()} bgcolor={color} />
-          ))}
-        </Stack>
-      </Paper>
-    </Stack>
+    <Paper elevation={8}>
+      <Stack
+        direction="row"
+        spacing={2}
+        p={1}
+        sx={{
+          bgcolor: "primary.main",
+          borderRadius: 1,
+        }}
+      >
+        {fillSlots().map(color => (
+          <ColorBall key={uuid()} bgcolor={color} />
+        ))}
+      </Stack>
+    </Paper>
   );
 };
 
--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,4 +1,4 @@
-import { Grid2, Grid2Props, Paper, styled } from "@mui/material";
+import { Grid2, Grid2Props, Paper, styled, Typography } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
 
 type Props = {
@@ -6,13 +6,13 @@ type Props = {
 };
 
 const StyledPanel = styled((props: Grid2Props) => (
-  <Grid2 {...props} size={{ xs: 12, md: 4 }}>
+  <Grid2 {...props} size={{ xs: 12, md: 4 }} component="section">
     {props.children}
   </Grid2>
 ))(({ theme }) => ({
   boxShadow: "inset 7px 7px 14px #bababa,inset -7px -7px 14px #ffffff",
   height: "100%",
-  padding: theme.spacing(1),
+  padding: theme.spacing(0.5),
   backgroundColor: "linear-gradient(145deg, #f0f0f0, #cacaca)",
   borderRadius: theme.spacing(1),
 }));
@@ -20,7 +20,18 @@ const StyledPanel = styled((props: Grid2Props) => (
 const Panel: FC<PropsWithChildren<Props>> = ({ children, title }) => {
   return (
     <StyledPanel>
-      <Paper sx={{ textAlign: "center" }}>{title}</Paper>
+      <Paper
+        elevation={2}
+        sx={{
+          textAlign: "center",
+          bgcolor: "transparent",
+          borderRadius: 0,
+        }}
+      >
+        <Typography fontWeight="fontWeightBold" variant="h6">
+          {title}
+        </Typography>
+      </Paper>
       {children}
     </StyledPanel>
   );
